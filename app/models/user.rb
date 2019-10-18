095a1e41489e2774084bcbc4b2c8d0eb10990ef2
Abhilash <nagisetty987@gmail.com>
2019-10-18 18:23:14 +0530

    image init

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,16 @@ class User < ApplicationRecord
 
   before_create :create_unique_id
 
-
   def create_unique_id
   	self.unique_id = SecureRandom.hex(10)
   	return true
+  end
+
+    @@versions = {
+      :square => [300, 300]
+  }
+
+  def self.versions
+    @@versions
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module ResumeBuilder
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+   	config.aws_temp_path = Rails.root.join('public', 'temp-uploads')
+
+   	
+
   end
 end
